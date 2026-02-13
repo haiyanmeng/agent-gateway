477bd2526060f514e50c386869cd418f70ca6622
Haiyan Meng <haiyanmeng@google.com>
2026-02-13 16:53:06 +0000

    Add adk-agent without gateway and calling mcp servers directly

---
+++ adk-agent-without-gateway/mcp_agent/agent.py
@@ -0,0 +1,54 @@
+# Copyright 2025 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+
+# Add these lines to configure logging
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+try:
+    local_mcp = McpToolset(
+        connection_params=StreamableHTTPConnectionParams(
+            url=f"http://server1-svc.mcp-server-calculator-system.svc.cluster.local:9000/mcp",
+        ),
+    )
+    logger.info("McpToolset local_mcp initialized successfully.")
+except Exception as e:
+    logger.error(f"Error initializing McpToolset local_mcp: {e}")
+
+try:
+    remote_mcp = McpToolset(
+        connection_params=StreamableHTTPConnectionParams(
+            url=f"https://mcp.deepwiki.com:443/mcp",
+        ),
+    )
+    logger.info("McpToolset remote_mcp initialized successfully.")
+except Exception as e:
+    logger.error(f"Error initializing McpToolset remote_mcp: {e}")
+
+root_agent = LlmAgent(
+    model="gemini-2.5-flash",
+    name="github_assistant_agent",
+    # instruction="""You are my GitHub repository assistant.
+    # Use the provided tools to help me manage my GitHub repositories.
+    # No need to ask permission from the user to use the tools. Just use them as needed.""",
+    tools=[local_mcp, remote_mcp],
+)
